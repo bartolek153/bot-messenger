240f479c92fe4ea436f7490556171c4b35ba4a9f
Luiz Eduardo Bartolassi <luizekbartolassi@gmail.com>
2023-04-08 01:18:06 -0300

    installation instructions

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
+git clone -b main https://github.com/bartolek153/bot-messenger.git
+
+cp credentials.ini bot-messenger/
+cd bot-messenger
+
 IMAGE_NAME=bot-messenger:$(git describe --abbrev=0 --tags)
 
 docker build -t $IMAGE_NAME .
-docker run -d --rm --mount $(pwd):/app $IMAGE_NAME --name telegram-bot
+docker run -d --rm -v $(pwd):/app --name bot $IMAGE_NAME
